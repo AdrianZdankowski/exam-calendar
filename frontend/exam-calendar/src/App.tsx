2dbb8a380b76284fb531ceb483c85ffe15f70ae4
AdrianZdankowski <adrianzdankowski@gmail.com>
2025-07-12 18:44:33 +0200

    feat: add login page component

--- frontend/exam-calendar/src/App.tsx
+++ frontend/exam-calendar/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element = {<MainLayout/>}>
         <Route index element = {<Home/>}/>
         <Route path="login" element = {<LoginPage/>}/>
+        <Route path="register" element = {<RegisterPage/>}/>
         </Route>
       </Routes>
     </Router>
