1d87af8987fd99df76888f5d3475c55fddd04164
Adrian Zdankowski <adrianzdankowski@gmail.com>
2025-07-25 16:49:04 +0200

    feat: add logout request to frontend

--- frontend/exam-calendar/src/components/Header.tsx
+++ frontend/exam-calendar/src/components/Header.tsx
@@ -1,17 +1,35 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import "./Header.css";
+import api from "../api/axios";
 
 const Header = () => {
 
   const {isAuthenticated, logout} = useAuth();
+
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      const response = await api.post('/auth/logout');
+      if (response.status === 200) {
+        console.log(response.statusText);
+        logout();
+        navigate('/', {replace: true});
+      }
+    } 
+    catch (error) {
+      console.error(error);
+    }
+    
+  };
 
   return (
     <header>
         <h2><Link className="clean-link" to="/">Exam calendar</Link></h2>
       <nav>
         {isAuthenticated ? 
-        <p onClick={logout}><Link className="clean-link" to="/">Logout</Link></p>
+        <p onClick={handleLogout}><Link className="clean-link" to=".">Logout</Link></p>
         :
         <p><Link className="clean-link" to="/login">Login</Link></p>
       }
