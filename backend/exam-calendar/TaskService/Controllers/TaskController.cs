d5eda04d9a3173123fdd2695499f4a665cda1c72
Adrian Zdankowski <adrianzdankowski@gmail.com>
2025-08-03 18:34:26 +0200

    create task service project

---
+++ backend/exam-calendar/TaskService/Controllers/TaskController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> test()
+        {
+            return Ok("Task controller is working");
+        }
+    }
+}
